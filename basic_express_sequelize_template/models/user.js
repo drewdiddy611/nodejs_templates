3a60b1d82e78e9049e7c66677459772b6c98defc
Andrew Senner <me@andrewsenner.com>
2017-08-07 12:28:13 -0400

    Update user model to ES6

--- basic_express_sequelize_template/models/user.js
+++ basic_express_sequelize_template/models/user.js
@@ -1,16 +1,24 @@
 'use strict';
-module.exports = function(sequelize, DataTypes) {
-  var User = sequelize.define('User', {
-    fname: DataTypes.STRING,
-    lname: DataTypes.STRING,
-    username: DataTypes.STRING,
-    email: DataTypes.STRING
-  }, {
-    classMethods: {
-      associate: function(models) {
-        // associations can be defined here
-      }
-    }
-  });
-  return User;
-};
+
+class User extends Model {}
+
+// Initialize the model.
+User.init(
+	{
+		fname: DataTypes.STRING,
+		lname: DataTypes.STRING,
+		username: DataTypes.STRING,
+		email: DataTypes.STRING
+	},
+	{
+		// options
+	}
+);
+
+// Add class methods with:
+// User.[classMethod] = () => {}
+
+// Add instance methods with:
+// User.prototype.[classMethod] => () => {}
+
+module.exports = User;
